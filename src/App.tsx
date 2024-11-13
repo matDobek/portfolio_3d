f92eaf2136cf3d226478683d4ed5c8dd8c831720
Mateusz Dobek <mat.dobek@gmail.com>
2024-11-13 18:22:45 +0100

    rotating sphere

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import React, { useRef } from 'react';
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls, useHelper } from '@react-three/drei';
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Points, PointMaterial, OrbitControls, useHelper } from '@react-three/drei';
 import { GridHelper, PointLightHelper } from 'three';
+import { randInSphere } from '../utils/math.ts';
 import './App.css'
 
 function Scene() {
-  const pointLightRef = useRef();
+  // const pointLightRef = useRef();
+  // useHelper(pointLightRef, PointLightHelper, 1, "hotpink")
+  // <ambientLight ref={pointLightRef} intensity={1.00} />
+  // <gridHelper args={[200, 50]} />
+  //
 
-  useHelper(pointLightRef, PointLightHelper, 1, "hotpink")
+  let positions = randInSphere(5000, 100);
+
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10
+    ref.current.rotation.y -= delta / 15
+  });
 
   return (
     <>
-      <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, 10, 10]} />
-        <meshStandardMaterial />
-      </mesh>
-      <mesh position={[10, 0, 0]}>
-        <sphereGeometry args={[1, 10, 10]} />
-        <meshStandardMaterial />
-      </mesh>
+      <group rotation={[50, 50, Math.PI / 4]}>
+        <Points ref={ref} positions={positions} >
+          <PointMaterial size={0.1} sizeAttenuation={true} depthWrite={false} />
+        </Points>
+      </group>
 
-      <ambientLight ref={pointLightRef} intensity={1.00} />
-
-      <gridHelper args={[200, 50]} />
+      <ambientLight intensity={1.00} />
     </>
   );
 }
@@ -32,7 +38,7 @@ function App() {
     <>
       <h1>Hello</h1>
       <div id="canvas-container">
-        <Canvas>
+        <Canvas camera={{position: [50, 50, 50]}}>
           <Scene />
           <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
         </Canvas>
